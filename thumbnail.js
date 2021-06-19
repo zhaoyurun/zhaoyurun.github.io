327f711d7682250b005e77f25545f8347360d903
zhaoyurun <86000532+zhaoyurun@users.noreply.github.com>
2021-06-19 20:03:54 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
    targetImage.addEventListener("click", function() {
     alert("点击了图片");
 });
-   targetImage.classList.remove("small");
+   targetImage.classList.add("small");
   // 你的所有代码都应该写在这里
 });

7b8a02d6868b5ddc05bd13164b35727f4b0ab2eb
zhaoyurun <86000532+zhaoyurun@users.noreply.github.com>
2021-06-19 20:04:52 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    let targetImage = document.querySelector("#smart-image");
    targetImage.addEventListener("click", function() {
-    alert("点击了图片");
+    targetImage.classList.add("small");
 });
-   targetImage.classList.add("small");
   // 你的所有代码都应该写在这里
 });

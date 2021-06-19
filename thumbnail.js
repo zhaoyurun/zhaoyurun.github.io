cdc4988300b902a81b56c52a9f0e9126fc600ac5
zhaoyurun <86000532+zhaoyurun@users.noreply.github.com>
2021-06-19 20:20:59 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    let targetImage = document.querySelector("#smart-image");
    targetImage.addEventListener("click", function() {
-    targetImage.classList.remove("small");
-});
    if (targetImage.classList.contains("small")) {
-      targetImage.classList.remove("small");
+   targetImage.classList.remove("small");
 } else {
-   targetImage.classList.add("small");
+  targetImage.classList.add("small");
 }
+});
   // 你的所有代码都应该写在这里
 });

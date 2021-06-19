445b86c98cdb0a99793651abf48f7610bf661e05
zhaoyurun <86000532+zhaoyurun@users.noreply.github.com>
2021-06-19 20:17:32 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,5 +3,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
    targetImage.addEventListener("click", function() {
     targetImage.classList.remove("small");
 });
+   if (targetImage.classList.contains("small")) {
+      targetImage.classList.remove("small");
+} else {
+   targetImage.classList.add("small");
+}
   // 你的所有代码都应该写在这里
 });
